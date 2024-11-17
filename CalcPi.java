8575b45845ca608a7e32c37f9d88640fa1cf4198
AlonKeren01 <alon.keren01@post.runi.ac.il>
2024-11-17 11:38:52 +0200

    finish all

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,24 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int n = Integer.parseInt(args[0]);
+		Double approx = 1.0;
+		Double divisor = 3.0;
+		boolean action = false;
+
+		for (int i = 0; i < n; i++){
+			if (!action){
+				approx = approx - (1 / divisor);
+				action = true;
+			}
+			else {
+				approx = approx + (1 / divisor);
+				action = false;
+			}
+			divisor = divisor + 2;
+		}
+
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated:     " + approx * 4.0);
 	}
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,34 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int n = Integer.parseInt(args[0]);
+	    String mode = args[1]; // “v” (verbose) or “c” (concise).
+		boolean voc = true; //true is v, false is c
+		int steps = 1;
+		int j = 0;
+
+		if (mode.equals("c")) voc = false;
+
+		for (int i = 1; i < n + 1; i++){
+			j = i;
+			if (voc) System.out.print(j + " ");
+
+			while (true){
+				if (j % 2 == 0) j = j / 2;
+				else j = (j * 3) + 1;
+				steps += 1;
+
+				if (j == 1) {
+					if (voc) System.out.print(j + " ");
+					break;
+				}
+				else if (voc) System.out.print(j + " ");
+			}
+
+			if (voc) System.out.println("(" + steps + ")");
+			steps = 1;
+		}
+
+		System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
 	}
 }
